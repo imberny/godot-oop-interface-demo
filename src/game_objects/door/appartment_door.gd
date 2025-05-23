fafd203f449e8934e0696f5e9eeec018dba2bb7d
imberny <bernard.p.cloutier@gmail.com>
2025-05-22 21:27:34 -0400

    Fix scene init bug, rename folders

--- src/game_objects/door/appartment_door.gd
+++ src/game_objects/door/appartment_door.gd
@@ -4,11 +4,15 @@ extends Node3D
 @export var _starts_locked: bool:
 	set(value):
 		_starts_locked = value
+		if not is_inside_tree():
+			await ready
 		_key_lock.starts_locked = value
 
 @export var _starts_open: bool:
 	set(value):
 		_starts_open = value
+		if not is_inside_tree():
+			await ready
 		_door.starts_open = value
 
 @onready var _door: Door = $Door
---
+++ src/interfaces/hurtable.gd
@@ -0,0 +1,15 @@
+class_name Hurtable extends Interface
+
+#region boilerplate
+
+const INTERFACE_NAME := &"Hurtable"
+
+
+static func try_get(from: Node) -> Hurtable:
+	return try_get_interface(INTERFACE_NAME, from)
+
+
+func get_interface_name() -> StringName:
+	return INTERFACE_NAME
+
+#endregion boilerplate
